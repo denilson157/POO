b0ce3dc205b2c87bddec350a4c280a07a1b3ea20
Denílson Pereira <dpereira@actioncargo.com.br>
2021-04-06 22:25:16 -0300

    fim da aula

--- controleEstoque/estoque.php
+++ controleEstoque/estoque.php
@@ -2,12 +2,15 @@
 
 require('classes/Usuario.class.php');
 require('classes/Fabricante.class.php');
-
+require('classes/Estoque.class.php');
+require('classes/Movimentacao.class.php');
 
 class Main {
     public function __construct(){
         $objUsuario = new Usuario();
         $objFabricante = new Fabricante();
+        $objEstoque = new Estoque();
+        $objMovimentacao = new Movimentacao();
     }
 
     public function __destruct()
